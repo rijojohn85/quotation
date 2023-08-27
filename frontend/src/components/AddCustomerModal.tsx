262fb84f1dce988e5daa0a7cb605e6813cebbb0d
rijojohn85 <rijojohn85@gmail.com>
2023-08-27 11:26:59 +0530

    started work on the modal

--- frontend/src/components/AddCustomerModal.tsx
+++ frontend/src/components/AddCustomerModal.tsx
@@ -1,15 +1,20 @@
-import { Modal } from 'react-bootstrap';
+import { Form, Modal } from 'react-bootstrap';
 
 interface AddCustomerModalProps {
 	onDimiss: () => void;
 }
 const AddCustomerModal = ({ onDimiss }: AddCustomerModalProps) => {
+	const handleSubmit = () => {
+		console.log('submit');
+	};
 	return (
 		<Modal show={true} onHide={onDimiss}>
 			<Modal.Header closeButton>
 				<Modal.Title>Add Customer</Modal.Title>
 			</Modal.Header>
-			<Modal.Body>Body</Modal.Body>
+			<Modal.Body>
+				<Form onSubmit={handleSubmit}></Form>
+			</Modal.Body>
 		</Modal>
 	);
 };
