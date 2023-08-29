cbbe647c2faaa36a1738023da0a074f493cea5aa
rijojohn85 <rijojohn85@gmail.com>
2023-08-29 18:40:50 +0530

    added sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
 	return (
 		<>
-			<Header />
-			<main className="py-3">
-				<Container>
-					<Outlet />
-				</Container>
-			</main>
+			<Row>
+				<Col md={2}>
+					<Sidebar />
+				</Col>
+				<Col md={10}>
+					<main className="py-3">
+						<Container>
+							<Outlet />
+						</Container>
+					</main>
+				</Col>
+			</Row>
 			<ToastContainer />
 		</>
 	);
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,5 +1,70 @@
+import {
+	CDBSidebar,
+	CDBSidebarContent,
+	CDBSidebarFooter,
+	CDBSidebarHeader,
+	CDBSidebarMenu,
+	CDBSidebarMenuItem
+} from 'cdbreact';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<div
+			style={{
+				display: 'flex',
+				height: '100vh',
+				overflow: 'scroll initial'
+			}}
+		>
+			<CDBSidebar
+				textColor="#fff"
+				backgroundColor="#333"
+				className={''}
+				breakpoint={0}
+				toggled={false}
+				minWidth={''}
+				maxWidth={''}
+			>
+				<CDBSidebarHeader
+					prefix={<i className="fa fa-bars fa-large"></i>}
+				>
+					<Link
+						to="/"
+						className="text-decoration-none"
+						style={{ color: 'inherit' }}
+					>
+						Sidebar
+					</Link>
+				</CDBSidebarHeader>
+
+				<CDBSidebarContent className="sidebar-content">
+					<CDBSidebarMenu>
+						<NavLink to="/">
+							<CDBSidebarMenuItem icon="columns">
+								Home
+							</CDBSidebarMenuItem>
+						</NavLink>
+						<NavLink to="/customers">
+							<CDBSidebarMenuItem icon="user">
+								Customers
+							</CDBSidebarMenuItem>
+						</NavLink>
+					</CDBSidebarMenu>
+				</CDBSidebarContent>
+
+				<CDBSidebarFooter>
+					<div
+						style={{
+							padding: '20px 5px'
+						}}
+					>
+						Sidebar Footer
+					</div>
+				</CDBSidebarFooter>
+			</CDBSidebar>
+		</div>
+	);
 };
 
 export default Sidebar;
